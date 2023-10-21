6a4eea11e22f7952d07027b08ffa960f6ddf2ebb
dhoiffahmad <dhoiff.ahmad@gmail.com>
2023-10-21 15:49:45 +0700

    scroll top berfungsi

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -1,8 +1,15 @@
 // Show Scrool Top
-function scrollTop(){
-    const scrollT0p = document.getElementById('.scroll-top')
+function scrollTop() {
+    const scrollT0p = document.getElementById('scrollTop');
 
-    if(this.scrollY >= 560) scrollT0p.classList.add('show-scroll');
-    else scrollT0p.classList.remove('show-scroll')
-};
-console.log(scrollTop);
+    if (window.scrollY >= 10) {
+        scrollT0p.classList.add('show-scroll');
+    } else {
+        scrollT0p.classList.remove('show-scroll');
+    }
+}
+
+// Tambahkan event listener untuk memanggil fungsi scrollTop saat scroll
+window.addEventListener('scroll', scrollTop);
+
+console.log(scrollTop); // Ini akan mencetak definisi fungsi scrollTop, bukan memanggilnya
